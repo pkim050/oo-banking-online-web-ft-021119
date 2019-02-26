b5bb2e627ce5c8805ec1cb2b49ba7b1878c590d1
Patrick Kim <pkim050@gmail.com>
2019-02-26 23:26:02 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount
   attr_accessor :name, :balance
   
-  def initialize(name, balance = "1000")
+  def initialize(name, balance = 1000)
     @name = name
     @balance = balance
   end
