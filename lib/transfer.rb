6bbc391b3e44e29b02bc8189c10490ed32e26b4f
Patrick Kim <pkim050@gmail.com>
2019-02-27 01:07:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
   # your code here
-  attr_accessor :sender, :receiver, :amount, :status
+  attr_accessor :sender, :receiver, :amount, :status, :valid
   
   def initialize(sender, receiver, amount)
     @sender = sender
