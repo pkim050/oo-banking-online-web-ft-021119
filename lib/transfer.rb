5f8386aed5fb584b30186d71c0a833c48c77949d
Patrick Kim <pkim050@gmail.com>
2019-02-27 00:47:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
   # your code here
+  attr_accessor :name, :name2, :amount
+  
+  def initialize(name, name2, amount)
+    @name = name
+    @name2 = name2
+    @amount = amount
+  end
 end
